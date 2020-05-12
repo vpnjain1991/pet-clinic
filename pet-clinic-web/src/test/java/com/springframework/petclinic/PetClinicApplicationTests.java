89a200f1470276d6a7b8decb134cc4cbd54502f9
vipin.jain <vipin.jain@imaginea.com>
2020-05-13 00:44:55 +0530

    config changes closes #1

--- pet-clinic-web/src/test/java/com/springframework/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/springframework/petclinic/PetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package petclinic;
+package com.springframework.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
