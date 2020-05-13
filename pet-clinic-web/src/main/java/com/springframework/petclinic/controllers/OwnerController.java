1b2d4d6c0798517dc261c912f50e8a79a855a6bc
vipin.jain <vipin.jain@imaginea.com>
2020-05-13 16:55:37 +0530

    List all owners on Owners Index Page, closes #15

---
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.springframework.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String list(){
+        return "owners/index";
+    }
+}
