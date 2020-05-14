1bab6e9efcdebc331a4850d2a44d000e3e833a14
vipin.jain <vipin.jain@imaginea.com>
2020-05-14 15:25:58 +0530

    added list vets, owners list code, closes #14, #15

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.springframework.petclinic.controllers;
 
+import com.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.model.IModel;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String list(){
+    public String list(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
