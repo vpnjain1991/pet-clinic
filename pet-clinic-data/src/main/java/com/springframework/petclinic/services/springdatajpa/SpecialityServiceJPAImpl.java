6357a322ea16d544a6da6865598d8b35c212dbf3
vipin.jain <vipin.jain@imaginea.com>
2020-05-16 16:03:07 +0530

    Created Spring Data Speciality Service, closes #44

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/springdatajpa/SpecialityServiceJPAImpl.java
@@ -0,0 +1,47 @@
+package com.springframework.petclinic.services.springdatajpa;
+
+import com.springframework.petclinic.model.Speciality;
+import com.springframework.petclinic.repositories.SpecialityRepository;
+import com.springframework.petclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile(value = "springdatajpa")
+public class SpecialityServiceJPAImpl implements SpecialityService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialityServiceJPAImpl(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
