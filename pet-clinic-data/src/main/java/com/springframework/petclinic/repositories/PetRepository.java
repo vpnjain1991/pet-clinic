e16c5938b0f3df55233508b3f66c8a2cd6f5f4c8
vipin.jain <vipin.jain@imaginea.com>
2020-05-16 15:27:29 +0530

    Added Spring Data JPA Respositories, closes #38

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/repositories/PetRepository.java
@@ -0,0 +1,8 @@
+package com.springframework.petclinic.repositories;
+
+import com.springframework.petclinic.model.Owner;
+import com.springframework.petclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetRepository extends CrudRepository<Pet, Long> {
+}
