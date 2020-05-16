a3d538d7d367fa7d6e79f96f4d4bffa9baf4e5a0
vipin.jain <vipin.jain@imaginea.com>
2020-05-16 14:18:21 +0530

    Convert Vets to JPA Entities, closes #36

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
@@ -1,5 +1,10 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
     private String description;
 
