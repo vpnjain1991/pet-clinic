342492d7b6b5ba34a7562b1bc5730dbeb52ce740
vipin.jain <vipin.jain@imaginea.com>
2020-05-16 14:30:27 +0530

    Created Visit Entity, closes #25

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.springframework.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -15,6 +17,9 @@ public class Pet extends BaseEntity{
     private Owner owner;
     private LocalDate birthDate;
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public PetType getPetType() {
         return petType;
