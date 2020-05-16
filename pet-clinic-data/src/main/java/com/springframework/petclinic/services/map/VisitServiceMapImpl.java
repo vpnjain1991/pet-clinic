f4a42e4470d84b12d47ace3858485577c265bc5a
vipin.jain <vipin.jain@imaginea.com>
2020-05-16 19:29:46 +0530

    Created Visit Map Service, closes #45

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/VisitServiceMapImpl.java
@@ -0,0 +1,39 @@
+package com.springframework.petclinic.services.map;
+
+import com.springframework.petclinic.model.Visit;
+import com.springframework.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMapImpl extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
