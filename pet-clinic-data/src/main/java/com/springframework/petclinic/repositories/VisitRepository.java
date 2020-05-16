77f97675403e8bafd7a3c1bc6fa2bcbe6db0d8c4
vipin.jain <vipin.jain@imaginea.com>
2020-05-16 19:36:00 +0530

    Created Spring Data JPA Service for Visit, closes #46

--- pet-clinic-data/src/main/java/com/springframework/petclinic/repositories/VisitRepository.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/repositories/VisitRepository.java
@@ -1,7 +1,7 @@
 package com.springframework.petclinic.repositories;
 
-import com.springframework.petclinic.model.Vet;
+import com.springframework.petclinic.model.Visit;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VisitRepository extends CrudRepository<Vet, Long> {
+public interface VisitRepository extends CrudRepository<Visit, Long> {
 }
